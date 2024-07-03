4bf682e79409fb5887b2e77dd6b35d6f13855108
晦还明Dawn <2972682076@qq.com>
2024-07-03 16:59:22 +0800

    使用泛型

--- ArrayReverse/ArrayReverse.java
+++ ArrayReverse/ArrayReverse.java
@@ -1,38 +1,43 @@
+import java.lang.reflect.Array;
+
 /**
  * 测试如何反转一个整形数列
+ * 
  * @author Dawn
- * @version 1.0
+ * @version 1.1
  */
-public class ArrayReverse {
+public class ArrayReverse<T> {
     /**
      * 用于反转一个整形的数列
+     * 
      * @param arr 一个整形的数列，长度无限制
      * @return 返回一个反转后的整形数列
      */
-    public static int[] reverse(int[] arr) {// 改进方法：使用泛型使得方法适用于所有类型的数列
-        int[] reversed = new int[arr.length];
-        int len = arr.length;
-        for (int i : arr) {
-            reversed[--len] = i;
+    public static <T> T[] reverse(T[] arr) {
+        T[] reversed = (T[]) Array.newInstance(arr.getClass().getComponentType(), arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
         }
         return reversed;
     }
 
     /**
      * 用于输出一个数列的元素，按照从头到尾的顺序，元素之间以一个空格隔开，输出结束后换行
+     * 
      * @param arr 一个整形的数列，长度无限制
      */
-    public static void printArray(int[] arr) {// 改进方法：使用泛型使得方法适用于所有类型的数列
-        for (int i : arr) {
+    public static <T> void printArray(T[] arr) {
+        for (T i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        Integer arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
         printArray(arr);
-        int[] reversedArr = reverse(arr);
+        Integer[] reversedArr = reverse(arr);
         printArray(reversedArr);
+
     }
 }
